c5b842f952ee1abb0a6db9b50277ac42b5fa3418
Michael Blohm <mikeblohm@gmail.com>
2016-08-19 00:36:25 -0400

    Changed particle emission to be periodic and random around the perimeter of the face.

--- fabulous.js
+++ fabulous.js
@@ -1,12 +1,48 @@
-function onMouseDown(event) {
-	emitParticle(event.point, event.timeStamp);
+// Configuration
+var emissionFrequency = 0.25;
+var particleColor = 'red';
+var scaleRate = 1.05;
+var particleSize = 5;
+var particleDuration = 0.5;
+
+// State
+var lastEmissionTime = 0;
+var faceRectangles = [];
+
+function onFrame(event) {
+	if (event.time - lastEmissionTime > emissionFrequency) {
+		for (var i = 0; i < faceRectangles.length; i++) {
+			var point = randomPerimeterPointOnRectangle(faceRectangles[i]);
+			emitParticle(point, event.time);
+		}
+		lastEmissionTime = event.time;
+	}
 }
 
-var particles = [];
+function onMouseMove(event) {
+	// This mocks the presence of a detected face.
+	faceRectangles = [new Rectangle(event.point.x - 50, event.point.y - 50, 100, 100)];
+}
 
-function emitParticle(location, timeStamp) {
-	var particle = Path.Circle(location, 50);
-	particle.fillColor = 'red';
+function emitParticle(point, timeStamp) {
+	var particle = Path.Circle(point, particleSize);
+	particle.fillColor = particleColor;
 	particle.timeStamp = timeStamp;
-	particles.push(particle);
+	particle.onFrame = function(event) {
+		particle.scale(scaleRate);
+		if (event.time > particleDuration) particle.remove();
+	}
+}
+
+function randomPerimeterPointOnRectangle(rectangle) {
+	var random = Point.random();
+	if (random.x < 0.25) {
+		return new Point(rectangle.topLeft.x, rectangle.y + random.y * rectangle.height);
+	} else if (random.x < 0.5) {
+		return new Point(rectangle.x + random.y * rectangle.width, rectangle.topLeft.y);
+	} else if (random.x < 0.75) {
+		return new Point(rectangle.bottomRight.x, rectangle.y + random.y * rectangle.height);
+	} else {
+		return new Point(rectangle.x + random.y * rectangle.width, rectangle.bottomRight.y);
+	}
 }
