5daaaaf7ad102ae73ffd0ca2bf7db4afc6340a22
Zach Perrault <zach.perrault@gmail.com>
2016-08-28 16:04:54 -0500

    Decode websocket frames

--- client/elm/FacesApp.elm
+++ client/elm/FacesApp.elm
@@ -2,25 +2,58 @@ module FacesApp exposing (..)
 
 import Html exposing (..)
 import Html.App as Html
-import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Decode exposing
+  ( Decoder
+  , (:=)
+  , decodeString
+  , int
+  , object5
+  , string
+  , list
+  )
 import WebSocket
 
+type alias Face =
+  { x             : Int
+  , y             : Int
+  , height        : Int
+  , width         : Int
+  , matchCount    : Int
+  }
+
 type alias Model =
-  { frame : String }
+  { faces : List Face
+  , error : String
+  }
 
 type Msg
   = SocketFrame String
 
+decodeFace =
+  object5 Face
+    ( "x"          := int )
+    ( "y"          := int )
+    ( "height"     := int )
+    ( "width"      := int )
+    ( "matchCount" := int )
+
+decodeFrame =
+  ( "faces" := (list decodeFace) )
+
 init : ( Model, Cmd Msg )
 init =
-  ( Model "Waiting for server...", Cmd.none )
+  ( Model [] "Waiting for server...", Cmd.none )
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     SocketFrame frame ->
-      ( Model frame, Cmd.none )
+      case decodeString decodeFrame frame of
+        Err msg ->
+          ( { model | error = msg }, Cmd.none )
+        Ok faces ->
+          ( { model | faces = faces }, Cmd.none )
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -29,12 +62,13 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
   div []
-    [ pre [] [ text model.frame ] ]
+    [ pre [] [ text (toString (List.length model.faces)) ]
+    , pre [] [ text model.error ]]
 
 main =
   Html.program
-    { init = init
-    , view = view
-    , update = update
+    { init          = init
+    , view          = view
+    , update        = update
     , subscriptions = subscriptions
     }
